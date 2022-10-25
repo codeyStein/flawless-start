d8b00a5591f30e358ceff89471918fbc47bdf266
codeyStein <san.26.2010@icloud.com>
2022-10-25 09:51:09 -0600

    Add Date & Time

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import './App.css'
 
 function App() {
 
+const d = new Date();
+//Date
+const months = ["Jan.", "Feb.", "Mart.", "April.", "May.", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."]
+const days = ["Sun.", "Mon.", "Thu.", "Wed.", "Tue.", "Fri.", "Sat"];
+
+const day = days[d.getDay()]
+const dayNum = d.getDate()
+const month = months[d.getMonth()]
+const date = `${day} ${month} ${dayNum}`
+
+// Time
+const hour = d.getHours();
+const min = d.getMinutes();
+const time = `${hour<10 ? "0"+hour : hour}:${min<10 ? "0"+min : min}`
 
 
   return (
     <div className="App">
 
       <div className='widgets__left'>
-        <h2 className='widget--time'>00:00</h2>
-        <h2 className='widget--date'>Null. Null. NaN</h2>
+        <h2 className='widget--time'>{time}</h2>
+        <h2 className='widget--date'>{date}</h2>
       </div>
 
       <div className='widgets__right'>

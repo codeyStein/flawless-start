bb05b4372cd1dd81afa7cd5703a9ed80169d0f7f
codeyStein <san.26.2010@icloud.com>
2022-10-28 15:24:55 -0600

    Migrate Config to JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
-import { FaGithub, FaDev, FaYoutube } from 'react-icons/fa';
-import Config from './config'
+import  * as FaIcons  from 'react-icons/fa';
+import conf from '../config'
 
 function App() {
 
+
 // config variables
-const conf = Config[0]
-const timeDate = conf.widgets.timeDate
-const searchBar = conf.widgets.searchBar
-const quickActions = conf.widgets.quickActions
-const theme = conf.theme
+const timeDate = conf.widgets["Time and Date"]
+const searchBar = conf.widgets["Search Bar"]
+const quickActions = conf.widgets["Quick Actions"]
+const theme = conf["theme"]
+console.log(theme)
 
 const d = new Date();
 //Date
@@ -36,14 +37,14 @@ rs.setProperty('--primary', primary)
 rs.setProperty('--secondary', secondary)
 rs.setProperty('--bg', bg)
 
-const searchProvider = searchBar.searchProvider
+const searchProvider = searchBar["Search Provider"]
 
 
   return (
     <div className="App">
 
       <div className='main'>
-        { timeDate.visibility==='show' &&
+        { timeDate["visibility"]==='show' &&
       <div className='widgets__section-one'>
         <h2 className='widget--time'>{time}</h2>
         <h2 className='widget--date'>{date}</h2>
@@ -51,15 +52,15 @@ const searchProvider = searchBar.searchProvider
         }
 
       <div className='widgets__section-two'>
-        { quickActions.visibility==='show'  &&
+        { quickActions["visibility"]==='show'  &&
         <div className='widget--quick_actions'>
-          <div className='widget--quick_action--card'> <FaGithub /> </div>
-          <div className='widget--quick_action--card'> <FaDev /> </div>
-          <div className='widget--quick_action--card'> <FaYoutube /> </div>
+          <div className='widget--quick_action--card'> <FaIcons.FaGithub /> </div>
+          <div className='widget--quick_action--card'> <FaIcons.FaDev /> </div>
+          <div className='widget--quick_action--card'> <FaIcons.FaYoutube /> </div>
         </div>
         }
         {
-        searchBar.visibility==='show' &&
+        searchBar["visibility"]==='show' &&
         <form className='widget--search' action={`${
         searchProvider==="google" ? "https://google.com/search" : 
         searchProvider==="bing" ? "https://www.bing.com/search?form=MOZLBR&pc=MOZI&q" :
